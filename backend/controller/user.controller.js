f392a1c7d1847e7ed0aec63dd8e28c393a855a95
Prashant Fule <tbs.prasanth@gmail.com>
2024-09-18 14:24:13 +0530

    createuser API is done

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,18 @@
-const createUser = (req, res) => {
-  console.log(req.body);
+import userModel from "../model/user.controller.js";
+
+const createUser = async (req, res) => {
+  try {
+    const { name, fathername, email, phone } = req.body;
+
+    const newUser = new userModel({ name, fathername, email, phone });
+    await newUser.save();
+
+    res
+      .status(200)
+      .json({ success: false, message: "User created succesfully", newUser });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
 };
 
 export { createUser };
